da5bbed8744f66496568fc1ca52f4f7783cc739e
David Palacios <davidpal3c@gmail.com>
2024-06-19 17:21:11 -0600

    api request failed exception / load local

--- api/fetch.py
+++ api/fetch.py
@@ -8,6 +8,28 @@ import os
 from time import time, sleep
 
 
+
+def read_json_and_return(filename): 
+    """
+    Reads JSON data from a file or returns None if the file is not found.
+
+    Args:
+        filename (str): The path to the JSON file.
+
+    Returns:
+        dict: The parsed JSON data, or None if the file is not found.
+    """
+
+    try:
+        with open(filename, 'r') as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        print(f"Error: JSON file '{filename}' not found. ")
+        return None
+
+
+
 def api_exec():
     """
     Fetch the latest cryptocurrency data from CoinMarketCap API, process it into a pandas DataFrame,
@@ -43,45 +65,70 @@ def api_exec():
         response = session.get(url, params=parameters)
         data = json.loads(response.text)
 
+        # Save the JSON response to a file
+        file_path = 'data.json'
+        with open(file_path, 'w') as file:
+            json.dump(data, file)
+            
+
+        # Process data
+        # Configure pandas display options: max columns, values/decimal places, 
+        pd.set_option('display.max_columns', None)
+        pd.set_option('display.float_format', lambda x: '%.2f' % x)
+
+        # Normalize JSON data into a DataFrame
+        df = pd.json_normalize(data['data'])
+        df['timestamp'] = pd.to_datetime('now')
+
+        # Concatenate DataFrame with itself for demonstration purposes
+        # df = pd.concat([df2, df2], ignore_index=True)
+        df['timestamp'] = df['timestamp'].astype(str)  # Convert Timestamp to string
+
+
+        # Convert DataFrame to a list of dictionaries
+        crypto_data = df.to_dict(orient='records')
+
+        # Append the DataFrame to a CSV file
+        csv_path = 'api.csv'
+        if not os.path.isfile(csv_path):
+            df.to_csv(csv_path, header=True, index=False)
+        else:
+            df.to_csv(csv_path, mode='a', header=False, index=False)
+
+        return crypto_data
+    
+    # for i in range(333):
+    #     print(api_exec())
+    #     print('\nAPI execution completed')
+    #     sleep(60)  # Wait for 60 seconds before the next iteration
+
+    # exit()
+
     except (ConnectionError, Timeout, TooManyRedirects) as e:
-        print(e)
-        return []
+        print(f'Error: API request failed: {e}')
+    
+        file_path = 'data.json'
+        #Load local data
+        data = read_json_and_return(file_path)
+        if data:
+            print("API unreachable, using saved data from data.json")
 
+            
+            pd.set_option('display.max_columns', None)
+            pd.set_option('display.float_format', lambda x: '%.2f' % x)
 
+            df = pd.json_normalize(data['data'])
+            df['timestamp'] = pd.to_datetime('now')
 
-    # Save the JSON response to a file
-    file_path = 'data.json'
-    with open(file_path, 'w') as file:
-        json.dump(data, file)
+            df['timestamp'] = df['timestamp'].astype(str)  # Convert Timestamp to string
 
-    # Configure pandas display options: max columns, values/decimal places, 
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.float_format', lambda x: '%.2f' % x)
+            # Convert DataFrame to a list of dictionaries
+            crypto_data = df.to_dict(orient='records')
+            return crypto_data
+        
+        else:
+            print("Both API and saved data unavailable.")
+            return None    
+        
 
-    # Normalize JSON data into a DataFrame
-    df = pd.json_normalize(data['data'])
-    df['timestamp'] = pd.to_datetime('now')
-
-    # Concatenate DataFrame with itself for demonstration purposes
-    # df = pd.concat([df2, df2], ignore_index=True)
-    df['timestamp'] = df['timestamp'].astype(str)  # Convert Timestamp to string
-
-
-    # Convert DataFrame to a list of dictionaries
-    crypto_data = df.to_dict(orient='records')
-
-    # Append the DataFrame to a CSV file
-    csv_path = 'api.csv'
-    if not os.path.isfile(csv_path):
-        df.to_csv(csv_path, header=True, index=False)
-    else:
-        df.to_csv(csv_path, mode='a', header=False, index=False)
-
-    return crypto_data
-
-# for i in range(333):
-#     print(api_exec())
-#     print('\nAPI execution completed')
-#     sleep(60)  # Wait for 60 seconds before the next iteration
-
-# exit()
+    
--- api/views.py
+++ api/views.py
@@ -5,43 +5,59 @@ from django.http import JsonResponse
 from .fetch import api_exec
 
 import requests
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 
 
-def api_exec2():
-    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-    headers = {
-        'Accepts': 'application/json',
-        'X-CMC_PRO_API_KEY': 'c6b393db-6b8c-4faf-8cb5-4e918e116472',
-    }
-    response = requests.get(url, headers=headers)
-    data = response.json()
-    return data['data']
 
+# def api_exec2():
 
-def current_df(request):
+#     try:
+#         url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+#         headers = {
+#             'Accepts': 'application/json',
+#             'X-CMC_PRO_API_KEY': 'c6b393db-6b8c-4faf-8cb5-4e918e116472',
+#         }
+#         response = requests.get(url, headers=headers)
+#         data = response.json()
     
-    if 'crypto_data' not in request.session:
-        data = api_exec()
-        print(json.dumps(data, indent=2))
-        request.session['crypto_data'] = data
-    else:
-        data = request.session['crypto_data']
+#     except (ConnectionError, Timeout, TooManyRedirects) as e: 
+        
+#         file_path = 'data.json'
+
+#         with open(file_path, 'r') as file:
+#             data = json.load(file)
+
+#             print(f"LOCAL DATA:\n {data}")
+#             return data['data']
 
 
-    # data = json.dumps(df.to_dict())
-    context = {'json_data': data}
-
-    return render(request, 'api/current_df.html', context)
-
+#     return data['data']
 
 
 # def current_df(request):
     
-#     data = api_exec()
-#     print(json.dumps(data, indent=2))
+#     if 'crypto_data' not in request.session:
+#         data = api_exec2()
+#         print(json.dumps(data, indent=2))
+#         request.session['crypto_data'] = data
+#     else:
+#         data = request.session['crypto_data']
+
 
 #     # data = json.dumps(df.to_dict())
 #     context = {'json_data': data}
-    
 
 #     return render(request, 'api/current_df.html', context)
+
+
+
+def current_df(request):
+    
+    data = api_exec()
+    # print(json.dumps(data, indent=2))
+
+    # data = json.dumps(df.to_dict())
+    context = {'json_data': data}
+    
+
+    return render(request, 'api/current_df.html', context)
