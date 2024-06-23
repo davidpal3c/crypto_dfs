441e429fa8f4569dbb58ac7226f5834c4d23cd4c
David Palacios <davidpal3c@gmail.com>
2024-06-22 22:39:40 -0600

    --allow-empty

--- streamlit/pages/1_profile.py
+++ streamlit/pages/1_profile.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 
 st.write(' # Profile')
+
+# Currencies selected for review
--- streamlit/pages/2_dashboard.py
+++ streamlit/pages/2_dashboard.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 
 st.write(' # Dashboard')
+
+# Main application access
